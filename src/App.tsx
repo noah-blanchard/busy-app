dbc9cee4b5746fb8d3d546c6e7284c15a7350c56
Noah Blanchard <n.blanchard190302@gmail.com>
2025-04-19 15:51:59 -0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,9 @@ import '@lynx-js/web-core/index.css';
 import '@lynx-js/web-elements/index.css';
 import '@lynx-js/web-core';
 import '@lynx-js/web-elements/all';
-
 const App = () => {
   return (
-    <div>
-      <p>test</p>
-    </div>
+    <p>AH</p>
   );
 };
 
